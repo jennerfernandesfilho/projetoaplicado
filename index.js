1e801a461e0b6a85141e0d1a8da02527d62bb894
jennerfernandesfilho <157854557+jennerfernandesfilho@users.noreply.github.com>
2024-11-21 21:24:33 -0300

    Add or update the Azure App Service build and deployment workflow config

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-// index.js
-console.log('Olá, Mundo! Servidor iniciado...');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const message = 'Hello World from Node.js app!';
+
+  console.log(message);
+  res.send(
+    <html>
+      <head><title>Node.js App</title></head>
+      <body>
+        <h1>${message}</h1>
+        <script>
+          console.log('Mensagem recebida do servidor: ${message}');
+        </script>
+      </body>
+    </html>
+  );
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
