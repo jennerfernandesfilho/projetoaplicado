874d8398867ee947160e71dafff99fcb2f39928c
jennerfernandesfilho <jennerfernandesfilho@hotmail.com>
2024-11-25 14:29:33 -0300

    sprint 1

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
   );
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
